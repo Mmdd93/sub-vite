ca69bcd4e37e8eb208eb6b7a2b5709e367d0a347
M <157221259+Mmdd93@users.noreply.github.com>
2024-10-15 19:55:50 +0530

    Update Header.jsx

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -13,14 +13,7 @@ const Header = () => {
   return (
     <>
       <Container className="header pt-5">
-        <Row className="justify-content-center p-5" id="icon-list">
-          <div className="blur" />
-          {icons.map((icon, index) => (
-            <Col xs="auto" key={index}>
-              <FontAwesomeIcon size="2xl" icon={icon} />
-            </Col>
-          ))}
-        </Row>
+        
       </Container>
       <div className="justify-content-center d-flex pt-1 pb-5 title">
         <span className="text-span">{"!"}</span>
