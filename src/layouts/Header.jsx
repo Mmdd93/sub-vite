7c22cd2cfd81fa526159fddecd933a2e3881c3f8
M <157221259+Mmdd93@users.noreply.github.com>
2024-10-15 20:11:20 +0530

    Update Header.jsx

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,1 +1,26 @@
+import {
+  faArrowRightLong,
+  faGear,
+  faSliders,
+  faToggleOff,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Col, Container, Row } from "react-bootstrap";
 
+const Header = () => {
+  const icons = [faSliders, faToggleOff, faGear, faArrowRightLong];
+
+  return (
+    <>
+      <Container className="header pt-5">
+        
+      </Container>
+      <div className="justify-content-center d-flex pt-1 pb-5 title">
+        <span className="text-span">{"!"}</span>
+        <h1>{"به پنل کاربری خوش آمدید"}</h1>
+      </div>
+    </>
+  );
+};
+
+export default Header;
